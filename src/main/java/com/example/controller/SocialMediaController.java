1d1698f24c5aa945b0115a9e9ab730d63bcc3793
alexhshi <alexhshi@github.com>
2026-08-01 02:27:02 +0000

    Test run: 27/28 passed, 1 failed, 0 skipped - 2026-08-01T02:27:02.011Z

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -122,6 +122,7 @@ public class SocialMediaController {
       */
     //if (msgService.getMsgById(message_id).isPresent() && (!message_text.isBlank())) {
     //if (msgService.idExists(message_id) && (messageText.length() <= 255) && (messageText.length() > 0)) {
+    System.out.print(messageText);
     if (messageText.isEmpty()) {
       msgService.updateById(message_id, messageText);
       return 1;
